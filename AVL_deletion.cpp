1ad8d3cf584e8bd56f9fac74bed3e5c282036bca
Vighnesh Tiwari <vighneshtiwari16377@gmail.com>
2020-05-05 20:02:16 +0530

    Add files via upload

--- AVL_deletion.cpp
+++ AVL_deletion.cpp
@@ -8,6 +8,59 @@ struct Node
 };
 struct Node* root = NULL;
 
+int height(Node *root)
+{
+	if(root == NULL)
+	{
+		return 0;
+	}
+	return root->height;
+}
+
+int getBalance(Node* root)
+{
+	if(root == NULL)
+	{
+		return 0;
+	}
+	return height(root->left) - height(root->right);
+}
+
+Node *rightRotate(Node* root)
+{
+	Node *root_left = root->left;
+	Node *root_left_right = root_left->right;
+
+	//rotation
+	root_left->right = root;
+	root->left = root_left_right;
+
+	//update heights
+
+	root_left->height = max(height(root_left->left),height(root_left->right))+1;
+	root->height = max(height(root->left),height(root->right))+1;
+
+	return root_left;
+}
+
+Node *leftRotate(Node* root)
+{
+	Node *root_right = root->right;
+	Node *root_right_left = root_right->left;
+
+	//rotation
+	root_right->left = root;
+	root->right = root_right_left;
+
+	//update height
+
+	root_right->height = max(height(root_right->left),height(root_right->right))+1;
+	root->height = max(height(root->left),height(root->right))+1;
+
+	return root_right;
+}
+
+
 Node *insert(Node *root, int data)
 {
 	if(root == NULL)
@@ -31,6 +84,40 @@ Node *insert(Node *root, int data)
 	{
 		return root;
 	}
+
+	root->height = 1 + max(height(root->left),height(root->right));
+
+	int balance = getBalance(root);
+
+	//if this node becomes unbalanced then there are 4 cases
+
+	//left left case
+	if(balance > 1 and data < root->left->data)
+	{
+		return rightRotate(root);
+	}
+
+	//left right case 
+	if(balance > 1 and data > root->left->data)
+	{
+		root->left = leftRotate(root->left);
+		return rightRotate(root);
+	}
+	
+	//right right case
+	if(balance < -1 and data > root->right->data)
+	{
+		return leftRotate(root);
+	}
+
+	//right left case
+	if(balance < -1 and data < root->right->data)
+	{
+		root->right = rightRotate(root->right);
+		return leftRotate(root);
+	}
+
+	return root;
 
 	return root;
 }
@@ -83,6 +170,49 @@ Node* del(Node* root, int data)
 		root->right = del(temp, temp->data);
 	}
 
+	// If the tree had only one node 
+    // then return  
+    if (root == NULL)  
+    	return root;  
+  
+    // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE  
+    root->height = 1 + max(height(root->left),  
+                           height(root->right));  
+  
+    // STEP 3: GET THE BALANCE FACTOR OF  
+    // THIS NODE (to check whether this  
+    // node became unbalanced)  
+    int balance = getBalance(root);  
+  
+    // If this node becomes unbalanced,  
+    // then there are 4 cases  
+  
+    // Left Left Case  
+    if (balance > 1 &&  
+        getBalance(root->left) >= 0)  
+        return rightRotate(root);  
+  
+    // Left Right Case  
+    if (balance > 1 &&  
+        getBalance(root->left) < 0)  
+    {  
+        root->left = leftRotate(root->left);  
+        return rightRotate(root);  
+    }  
+  
+    // Right Right Case  
+    if (balance < -1 &&  
+        getBalance(root->right) <= 0)  
+        return leftRotate(root);  
+  
+    // Right Left Case  
+    if (balance < -1 &&  
+        getBalance(root->right) > 0)  
+    {  
+        root->right = rightRotate(root->right);  
+        return leftRotate(root);  
+    }
+
 	return root;
 }
 
@@ -98,18 +228,19 @@ void inOrder(Node *root)
 
 int main(int argc, char const *argv[])
 {
-	root = insert(root, 50);  
-    root = insert(root, 40);  
-    root = insert(root, 70);  
-    root = insert(root, 60);  
-    root = insert(root, 80);
-    root = insert(root, 75);  
-    root = insert(root, 85);  
-    root = insert(root, 90);  
+	root = insert(root, 9);  
+    root = insert(root, 5);  
+    root = insert(root, 10);  
+    root = insert(root, 0);  
+    root = insert(root, 6);  
+    root = insert(root, 11);  
+    root = insert(root, -1);  
+    root = insert(root, 1);  
+    root = insert(root, 2);  
 
     cout<<"Before Delete : ";
     inOrder(root);
-    root = del(root,16);
+    root = del(root, 10); 
    	cout<<"After Delete : ";
    	inOrder(root);
    	cout<<endl;
